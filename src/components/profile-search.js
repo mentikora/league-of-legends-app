7d1b7c34cb4865e967aa1d5256ee778c807d517e
horo <notender_boy@ukr.net>
2017-05-15 13:32:46 +0300

    update

--- src/components/profile-search.js
+++ src/components/profile-search.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 
-var ProfileSearch = React.createClass({
+class ProfileSearch extends React.Component{
+
+	constructor() {
+    super();
+    this.state = {
+      summonerData: null
+    };
+  }
 
 	onSearchButtonClick(){
-		let i = document.querySelector('.summoner_name');
-		console.log(i.value);
-	},
+		let key = "RGAPI-a1b83495-f7ac-4586-939d-31961ff89aa0";
+		let summonerName = document.querySelector('.search-field').value;
+		let URL = "https://eune.api.riotgames.com/api/lol/EUNE/v1.4/summoner/by-name/" + summonerName + "?api_key=" + key;
+		fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ summonerData: json });
+    });
+	}
 
 	render(){
 		return(
 			<div className="profile-search">
-				<input className="summoner-name" type="text" placeholder="Enter summoner name.." />
+				<input className="search-field" type="text" placeholder="Enter summoner name.." />
 				<span className="button button-search" onClick={this.onSearchButtonClick}>Search</span>
 			</div>
 		);
 	}
-});
+}
 
 export default ProfileSearch;
